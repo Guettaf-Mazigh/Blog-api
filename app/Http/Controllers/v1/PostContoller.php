2959aba98423a6d5c495caac01906e7c68b48165
Mazigh <guettafmazigh@gmail.com>
2026-02-02 20:43:18 +0100

    add Eloquent relationships between User and Post models & create PostResource

--- app/Http/Controllers/v1/PostContoller.php
+++ app/Http/Controllers/v1/PostContoller.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers\v1;
 
 use App\Http\Controllers\Controller;
-use App\Models\post;
+use App\Http\Resources\PostResource;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostContoller extends Controller
 {
     public function index(){
-        
+        return PostResource::collection(Post::with('author')->paginate(10));
     }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,8 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
     protected $fillable = ['title','content','user_id'];
+
+    public function author() : BelongsTo{
+        return $this->belongsTo(Post::class,'user_id');
+    }
 }

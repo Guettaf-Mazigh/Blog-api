780064511b997199489da46c007b977603da5c8f
Mazigh <guettafmazigh@gmail.com>
2026-02-13 23:14:42 +0100

    Fix ApprovalRequest migration and model; add user relationship in ApprovalRequest and approvalRequests method in User

--- app/Models/ApprovalRequest.php
+++ app/Models/ApprovalRequest.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ApprovalRequest extends Model
 {
-    protected $fillable = ['user_id','post_id','status'];
+    protected $fillable = ['user_id', 'post_id', 'status'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
--- database/migrations/2026_02_13_220714_create_approval_requests_table.php
+++ database/migrations/2026_02_13_220714_create_approval_requests_table.php
@@ -13,9 +13,13 @@ return new class extends Migration
     {
         Schema::create('approval_requests', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_is')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->foreignId('post_id')->constrained()->onDelete('cascade');
-            $table->enum('status',['approved,rejected,pending'])->default('pending');
+            $table->enum('status', [
+                'approved',
+                'rejected',
+                'pending',
+            ])->default('pending');
             $table->timestamps();
         });
     }
